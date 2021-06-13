5459ad7b833d8f6088609b1d8174cf7dc2d649b7
cdsanchezm <cdsanchezm@unbosque.edu.co>
2021-06-13 17:58:03 -0500

    form switch

--- js/SingUp.js
+++ js/SingUp.js
@@ -2,16 +2,29 @@
 
 console.log('funciona');
 var formulario = document.getElementById('formSing');
-var radioB1 = document.getElementById('inlineRadio1');
-var radioB2 = document.getElementById('inlineRadio2');
-var radioB3 = document.getElementById('inlineRadio3');
+var RadioB = document.getElementsByName('inlineRadioOptions');
+var RadioV;
 
 formulario.addEventListener('submit', function(e){
-        e.preventDefault();
-        var data = new FormData(formulario);
-        console.log(data.get('userName'));
-        console.log(data.get('password'));
-        console.log(data.get('RepeatPassword'));
-        console.log(data.get('mail'));
-        
+    e.preventDefault();
+    var data = new FormData(formulario);
+    console.log(data.get('userName'));
+    console.log(data.get('password'));
+    console.log(data.get('RepeatPassword'));
+    console.log(data.get('mail'));
+  
+      for(i=0; i<RadioB.length; i++){
+          if(RadioB[i].checked){
+             RadioV = RadioB[i].value;
+          }
+      }
+      
+      if(RadioV == "Propietario"){
+       location = ("/components/Property.html");  
+      }else if (RadioV == "Veterinario") {
+        location = ("/components/VeterinatiS.html");
+      } else if (RadioV == "Funcionario") {
+        location = ("/components/OfficialF.html");
+      }
+    
 })
