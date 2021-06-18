181db7af10f5b10abc2d790c06f76ee6806acd41
AndresGB <agalvisb@unbosque.edu.co>
2021-06-18 01:12:41 -0500

    Get owners Official Table

--- js/OwnerForm.js
+++ js/OwnerForm.js
@@ -2,16 +2,13 @@ var usernameOwner = sessionStorage.getItem("username")
 var passwordOwner = sessionStorage.getItem("password")
 var emailOwner = sessionStorage.getItem("email")
 var roleOwner = sessionStorage.getItem("role")
-var owner_id = 0;
+var person_id=Math.floor(Math.random() * 100000);
 var urlO =
   "http://localhost:8080/FourPawsCitizens-LazarusAES-25-1.0-SNAPSHOT/api/owners/"+usernameOwner;
 var formOwner = document.getElementById("formOwner");
-console.log(sessionStorage.getItem('username'));
 if(usernameOwner != null){
 
 formOwner.addEventListener("submit", function (e) {
-  owner_id ++;
-console.log(owner_id);
   e.preventDefault();
   var data = new FormData(formOwner);
    
@@ -23,7 +20,7 @@ console.log(owner_id);
       "password":passwordOwner,
       "email": emailOwner,
       "role": roleOwner,
-      "person_id":owner_id,
+      "person_id":person_id,
       "name":data.get('nameO'),
       "address":data.get('addressO'),
       "neighborhood": data.get('neigborhoodO'),
@@ -43,24 +40,11 @@ console.log(owner_id);
 }
 
 
-// funcion que lo lleva a la pagina de owner directamente 
-
-function validate_Owner (response){
-    if(roleOwner == "owner"){
-      window.location.href = "/components/owner.html"
-    }else{
-      alert("a ocurrido un problema, porfavor verifique su informacion");
-      location.reload();
-    }
-}
-
-//escogan la que les gusta mas
-
-
 //Funcion que lo lleva a la pagina publica
  
 function validate_Owner2 (response){
-  if(response == "owner"){
+ 
+  if(response  == "It was successfully saved by the owner"){
     alert("su Registro a sido exitoso");
     window.location.href = "/components/owner.html"
   }else{
